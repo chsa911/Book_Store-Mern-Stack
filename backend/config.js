bc1cb7f452ff1769ecd2c4e54962bd8f67ebbf63
chsa911 <ch.sa911@gmail.com>
2024-12-08 19:29:07 +0100

    new db

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb://localhost:27017/';
+  'mongodb+srv://chkocevar1:0vUEocjd9TUSDsPE@cluster0.o0xez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
