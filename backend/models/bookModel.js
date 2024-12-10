cf8def727e95ef9bc44bfb2b3fade11bb426503d
chsa911 <ch.sa911@gmail.com>
2024-12-10 15:09:55 +0100

    refactor bookmodel

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,73 +1,26 @@
 import mongoose from 'mongoose';
 
 const bookSchema = mongoose.Schema (
-  {
+    {
  /*bookfields1*/
-    authorB: {
-          type: String,
-          required: true,
-        },
-    keyw1: {
-      type: String,
-      required: true,
-    },
-    kw1Pos: {
-          type: Number,
-          required: true,
-        },
-    keyw2: {
-      type: String,
-      required: false,
-    },
-    kw2Pos: {
-          type: Number,
-          required: false,
-        },
-    keyw3: {
-      type: String,
-      required: false,
-    },
-    kw3Pos: {
-          type: Number,
-          required: false,
-        },
-    publisherB: {
-      type: String,
-      required: true,
-    },
-    pagesB: {
-      type: Number,
-      required: true,
-    },
-    reslt: {
-          type: String,
-          required: false,
-        },
-topTt: {
-          type: String,
-          required: false,
-        },
-/*entDat: {
-          type: Number,
-          required: false,
-        },
-voewrd: {
-          type: String,
-          required: false,
-        },
-voedatum: {
-          type: Number,
-          required: false,
-        },*/
-markBk: {
-          type: String,
-          required: false,
-        },
-
+    authorB: {type: String,},
+    keyw1: {type: String,},
+    kw1Pos: {type: Number,},
+    keyw2: {type: String,},
+    kw2Pos: {type: Number,},
+    keyw3: {type: String,},
+    kw3Pos: {type: Number,},
+    publisherB: {type: String,},
+    pagesB: {type: Number,},
+    reslt: {type: String,},
+    topTt: {type: String,},
+    markBk: {type: String,},
   },
   {
     timestamps: true,
   }
+
 );
+
 
 export const Book = mongoose.model('Book', bookSchema);
